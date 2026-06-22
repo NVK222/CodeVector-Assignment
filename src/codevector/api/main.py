924ba833270336177f3bc4a4dcb8aa108c5abba2
NVK222 <neerajvk2004@gmail.com>
2026-06-22 13:15:39 +0530

    Added filtering through categories

--- src/codevector/api/main.py
+++ src/codevector/api/main.py
@@ -1,4 +1,4 @@
-from typing import Annotated
+from typing import Annotated, Literal
 from fastapi import FastAPI, Query
 from codevector.api.schema import Product
 from codevector.supabase_client import supabase
@@ -12,24 +12,25 @@ async def check_health():
 
 
 @app.get("/api/items", response_model=list[Product])
-def get_paginated_products(
+def get_products(
     skip: int = 0,
     limit: Annotated[int, Query(ge=1, le=1000)] = 25,
+    category: Literal[
+        "Electronics", "Groceries", "Kitchen Appliances", "Food", "Pets", "Fashion"
+    ]
+    | None = None,
 ):
     """
-    Get paginated list of products
+    Get paginated list of products with filtering
     Args:
         skip: int = Starting index of products
         limit: int = Number of products to return
-
+        category: str = A category from (Electronics, Groceries, Kitchen Appliances, "Food", "Pets", "Fashion")
     Returns:
         A list of Product. (name, category, price, created_at, updated_at)
     """
-    data = (
-        supabase.table("products")
-        .select("*")
-        .range(skip, skip + limit - 1)
-        .execute()
-        .data
-    )
+    data = supabase.table("products").select("*")
+    if category is not None:
+        data = data.eq("category", category)
+    data = data.range(skip, skip + limit - 1).execute().data
     return data
